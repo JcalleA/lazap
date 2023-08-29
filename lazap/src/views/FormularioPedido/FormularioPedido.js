1b878da5d4645b0215fd570cdc599032beb7df6b
Jhonatan Calle <107059096+JcalleA@users.noreply.github.com>
2023-08-29 16:26:47 +0000

    Update FormularioPedido.js

--- lazap/src/views/FormularioPedido/FormularioPedido.js
+++ lazap/src/views/FormularioPedido/FormularioPedido.js
@@ -3,27 +3,79 @@ import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 
 
-const FormularioPedido = () => {
+const FormularioPedido = (props) => {
 
   const navigate=useNavigate();
 
-  const url='https://oauth.sandbox.nequi.com/oauth2/token?grant_type=client_credentials';
-  const config ={headers: { 
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Authorization': 'Basic MnM1MmVqMzQzODRpc3FwNjJrZDIwZm1nZDI6cjhldmxhMnVrcWJxaWV1am0ycmlqbzd2dm1uNmNkcjY5NnZndDRrZWxoaWRjM2ExNWwz'
+  
+
+        const token = async ()=>{
+          const url='https://oauth.sandbox.nequi.com/oauth2/token?grant_type=client_credentials';
+          const config ={headers: { 
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Authorization': 'Basic MnM1MmVqMzQzODRpc3FwNjJrZDIwZm1nZDI6cjhldmxhMnVrcWJxaWV1am0ycmlqbzd2dm1uNmNkcjY5NnZndDRrZWxoaWRjM2ExNWwz'
         }}
 
-  const pedir = async ()=>{
+           await axios.post(url,{},config)
+          .then((response) => {
+            console.log(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
 
-    const token = await axios.post(url,{},config)
-    .then((response) => {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  }
+          return (response.data.access_token)
+        }
+        const nequiMensaje=async () =>{
+          let fecha ='01/20';
+          let phone='321352541';
+          let valor ='32000';
+          let url = 'https://api.sandbox.nequi.com/payments/v2/-services-paymentservice-unregisteredpayment'
+          let data = JSON.stringify({
+            "RequestMessage": {
+              "RequestHeader": {
+                "Channel": "PNP04-C001",
+                "RequestDate": `${fecha}`,
+                "MessageID": "123456789016",
+                "ClientID": "12345767678",
+                "Destination": {
+                  "ServiceName": "PaymentsService",
+                  "ServiceOperation": "unregisteredPayment",
+                  "ServiceRegion": "C001",
+                  "ServiceVersion": "1.2.0"
+                }
+              },
+              "RequestBody": {
+                "any": {
+                  "unregisteredPaymentRQ": {
+                    "phoneNumber": `${phone}`,
+                    "code": "NIT_1",
+                    "value": `${valor}`,
+                    "reference1": "reference1",
+                    "reference2": "reference2",
+                    "reference3": "reference3"
+                  }
+                }
+              }
+            }
+          });
+          
+          let config = {
+            headers: { 
+              'Authorization': `Bearer ${token()}` , 
+              'Content-Type': 'application/json'
+            },
+          };
+          await axios.post(url,data,config)
+          .then((response) => {
+            console.log(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
 
+        }
+        
   useEffect(()=>{
     pedir();
 
@@ -34,6 +86,7 @@ const FormularioPedido = () => {
 
   return (
     <div>
+
       
       </div>
   );
